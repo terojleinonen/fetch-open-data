31ce404a553393529482a4663963fb1f9f2d52f1
Tero Leinonen <66237903+terojleinonen@users.noreply.github.com>
2024-12-10 12:07:59 +0200

    added try{} catch{} to handle errors +some minor tweaks

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,32 @@
 export default async function Page() {
-  let data = await fetch('http://rajapinnat.ymparisto.fi/api/Hakemistorajapinta/1.0/odata/Tutkl')
-  let posts = await data.json()
-  console.log(posts.value[0].Nimi)
+  console.log("application started...");
+  const source = 'http://rajapinnat.ymparisto.fi/api/Hakemistorajapinta/1.0/odata/Tutkl';
+  let data;
+
+  try {
+    const responce = await fetch(source);
+    if (!responce.ok){
+      throw new Error(`HTTP error: Status ${response.status}`);
+    }
+    data = await responce.json();
+
+  } catch (err) {
+    console.log(err);
+  }
+  
   return (
-    <ul>
-      {posts.value.map((post) => (
-        <li key={post.Nro}>Tutkl_Id: {post.Tutkl_Id} Nro: {post.Nro} Nimi: {post.Nimi} ICES_rlabo: {post.ICES_rlabo} AikaMuutos: {post.AikaMuutos}</li>
-      ))}
-    </ul>
+    <>
+      <div className="bg-slate-200 py-4">
+        <h1 className="text-4xl font-extrabold text-center">{source}</h1>
+        <input type="text" placeholder="Search..." autoComplete="off" className="text-3xl"/>
+      </div>
+      <div className="">
+        <ul className="my-4 text-lg text-gray-500">
+          {data.value.map((data) => (
+            <li key={data.Nro}>Tutkl_Id: {data.Tutkl_Id} Nro: {data.Nro} Nimi: {data.Nimi} ICES_rlabo: {data.ICES_rlabo} AikaMuutos: {data.AikaMuutos}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
