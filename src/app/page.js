99b2300ce478e12bc99e56b41293960cf1563835
Tero Leinonen <66237903+terojleinonen@users.noreply.github.com>
2024-12-11 15:37:30 +0200

    chanded api and added some new pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,21 @@
-import Filterfield from "./filterfield";  
-
+import Link from 'next/link'
 export default async function Page() {
-  console.log("application started...");
-  const source = 'http://rajapinnat.ymparisto.fi/api/Hakemistorajapinta/1.0/odata/Tutkl';
-  let data;
-
-  try {
-    const responce = await fetch(source);
-    if (!responce.ok){
-      throw new Error(`HTTP error: Status ${response.status}`);
-    }
-    data = await responce.json();
-
-  } catch (err) {
-    console.log(err);
-  }
+console.log("app started...")
   
   return (
     <>
-      <div className="py-4 bg-slate-200 dark:bg-slate-500 ">
-        <h1 className="text-4xl font-extrabold text-center dark:text-black">{source}</h1>
-        <Filterfield/>
-      </div>
-      <div className="">
-        <ul className="my-4 text-lg text-gray-500">
-          {data.value.map((data) => (
-            <li key={data.Nro}>Tutkl_Id: {data.Tutkl_Id} Nro: {data.Nro} Nimi: {data.Nimi} ICES_rlabo: {data.ICES_rlabo} AikaMuutos: {data.AikaMuutos}</li>
-          ))}
-        </ul>
-      </div>
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/books">Books</Link>
+      </li>
+      <li>
+        <Link href="/shorts">Shorts</Link>
+      </li>
+    </ul>
+      
     </>
   )
 }
---
+++ src/app/shorts/page.js
@@ -0,0 +1,46 @@
+
+export default async function Page() {
+    const url = 'https://stephen-king-api.onrender.com/api/shorts';
+    const headers = new Headers({
+      "User-Agent": "fetch-open-data/1.0"
+    });
+    const options = {
+      method: 'GET',
+      headers: headers
+    };
+    let data; 
+  
+    try {
+      const responce = await fetch(url,headers);
+      if (!responce.ok){
+        throw new Error(`HTTP error: Status ${response.status}`);
+      }
+      data = await responce.json();
+  
+    } catch (err) {
+      console.log(err);
+    }
+    console.log(data)
+    return (
+      <>
+        <div className="mb-8 py-4 bg-slate-200 dark:bg-slate-500 ">
+          <h1 className="text-xl font-extrabold text-center dark:text-black">{url}</h1>
+        </div>
+        <div className="">
+          <ul className="flex flex-row flex-wrap justify-evenly text-lg text-gray-500">
+            *{data.data.map((data) => (
+              <li key={data.id} className="p-4 mb-8 shadow-2xl">
+                id: {data.id}<br/>
+                title: {data.title}<br/>
+                type: {data.type}<br/>
+                originallyPublishedIn: {data.originallyPublishedIn}<br/>
+                collectedIn: {data.collectedIn}<br/>     
+                year: {data.year}<br/>
+                created_at: {data.created_at}<br/>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </>
+    )
+  }
