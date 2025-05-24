89cc86ea94a02d98ccb2e7df7d133716a0f7124a
Tero Leinonen <66237903+terojleinonen@users.noreply.github.com>
2025-05-24 22:18:28 +0300

    shows only non empty properties

--- src/app/pages/villains/page.js
+++ src/app/pages/villains/page.js
@@ -1,13 +1,14 @@
 
 import Request from "@/app/components/request";
 import Header from "@/app/components/header";
+import { UNDERSCORE_NOT_FOUND_ROUTE } from "next/dist/shared/lib/constants";
 
 export default async function Page() {
     const data = await Request('villains')
   
     return (
       <div>
-        <Header title="STEPHEN KING VILLAINS"/>
+        <Header title="STEPHEN KING VILLAINS"/>
         <div className="">
           <ul className="text-2xl">
             {data.data.map((data) => (
@@ -16,9 +17,17 @@ export default async function Page() {
                 <b>Name:</b> {data.name}<br/>
                 <b>Gender:</b> {(data.gender ===null)? "n/a":data.gender}<br/>
                 <b>Status:</b> {data.status}<br/>
-                <b>Notes:</b> {data.notes.map((notes)=> (notes ==="")? "n/a" : notes ).join(' | ')}<br/>
-                <b>Books:</b> {data.books.map((books)=> (books.title === "")? "n/a" : books.title).join(' | ')}<br/>
-                <b>Shorts:</b> {data.shorts.map((shorts)=> (shorts.title === "")? "n/a" : shorts.title).join(' | ')}
+                <b>Notes:</b> {data.notes.map((notes)=> (notes ==="")? "n/a" : notes ).join(' | ')}<br/>
+                <div>
+                  {
+                    (data.books.length === 0 | data.books === undefined)? "" :<div><b>Books:</b> {data.books.map((books)=> books.title).join(' | ')}<br/></div>
+                  }
+                </div>
+                <div>
+                  {
+                    (data.shorts.length === 0 | data.shorts === undefined)? "" :<div><b>Shorts:</b> {data.shorts.map((shorts)=> shorts.title).join(' | ')}</div>
+                  }
+                </div>
               </li>
             ))}
           </ul>
