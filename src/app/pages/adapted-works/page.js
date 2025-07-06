73911b8f0153dc46a46f9a3bd3c8e0c465fce361
Tero Leinonen <66237903+terojleinonen@users.noreply.github.com>
2025-07-06 04:17:54 +0300

    adapted works

--- src/app/pages/adapted-works/page.js
+++ src/app/pages/adapted-works/page.js
@@ -2,14 +2,23 @@ import Request from "@/app/components/request";
 import AdaptationListClient from "./AdaptationListClient"; // Import the new client component
 
 export default async function Page() {
-    const adaptationsData = await Request('adaptations');
+    // Fetch all necessary data in parallel
+    const [adaptationsData, booksData, shortsData] = await Promise.all([
+      Request('adaptations'),
+      Request('books'),
+      Request('shorts')
+    ]);
 
     return (
       <div
         className="page-background-text"
         style={{ '--page-background-text-content': "'ADAPTED WORKS'" }}
       >
-        <AdaptationListClient initialAdaptations={adaptationsData} /> {/* Pass data to client component */}
+        <AdaptationListClient 
+          initialAdaptations={adaptationsData} 
+          initialBooks={booksData}
+          initialShorts={shortsData}
+        />
       </div>
     )
   }
